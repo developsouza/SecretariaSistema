f995519503429d1633a2e724b6d711672b43f3f3
Gilney Souza <gilneysouza.dev@gmail.com>
2026-03-13 07:44:23 -0300

    feat: ajustar altura da logo na navbar para 95px e modificar estilos de botão

--- assembleia-jacuma/frontend/src/components/Navbar.jsx
+++ assembleia-jacuma/frontend/src/components/Navbar.jsx
@@ -69,7 +69,7 @@ export default function Navbar() {
             <nav className={`navbar ${navbarScrolled ? "scrolled" : ""}`}>
                 <div className="container navbar-inner">
                     <Link to="/" className="navbar-logo">
-                        <img src="/logo.png" alt="Logo Assembleia de Deus" style={{ height: "120px", width: "auto" }} />
+                        <img src="/logo.png" alt="Logo Assembleia de Deus" style={{ height: "95px", width: "auto" }} />
                     </Link>
 
                     <div className="navbar-links">
